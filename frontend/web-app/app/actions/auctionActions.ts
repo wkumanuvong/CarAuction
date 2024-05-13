83cd9e0e518bb5fb05c6928aa9ab9df584827e85
wkumanuvong <waleerat@msn.com>
2024-05-13 13:44:12 +0700

    End of section 10

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,14 +1,12 @@
 'use server';
 
 import { Auction, PagedResult } from '@/types';
-import { getTokenWorkaround } from './authActions';
+import { fetchWrapper } from '@/lib/fetchWrapper';
+import { FieldValues } from 'react-hook-form';
+import { revalidatePath } from 'next/cache';
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-  const res = await fetch(`http://localhost:6001/search${query}`);
-
-  if (!res.ok) throw new Error('Failed to fetch data');
-
-  return res.json();
+  return await fetchWrapper.get(`search/${query}`);
 }
 
 export async function updateAuctionTest() {
@@ -16,21 +14,26 @@ export async function updateAuctionTest() {
     mileage: Math.floor(Math.random() * 100000) + 1
   };
 
-  const token = await getTokenWorkaround();
-
-  const res = await fetch(
-    'http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',
-    {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: 'Bearer ' + token?.access_token
-      },
-      body: JSON.stringify(data)
-    }
+  return await fetchWrapper.put(
+    'auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',
+    data
   );
+}
 
-  if (!res.ok) return { status: res.status, message: res.statusText };
+export async function createAuction(data: FieldValues) {
+  return await fetchWrapper.post('auctions', data);
+}
 
-  return res.statusText;
+export async function getDetailedViewData(id: string): Promise<Auction> {
+  return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(data: FieldValues, id: string) {
+  const res = await fetchWrapper.put(`auctions/${id}`, data);
+  revalidatePath(`/auctions/${id}`);
+  return res;
+}
+
+export async function deleteAuction(id: string) {
+  return await fetchWrapper.del(`auctions/${id}`);
 }
---
+++ frontend/web-app/app/providers/ToasterProvider.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+export default function ToasterProvider() {
+  return <Toaster position="bottom-right" />;
+}
