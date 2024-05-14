0dad8c794d80ccd4df6b8da4c9c8af4c6df05235
wkumanuvong <waleerat@msn.com>
2024-05-14 19:59:09 +0700

    End of section 11

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -47,6 +47,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -54,6 +56,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
